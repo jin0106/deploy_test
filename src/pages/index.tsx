4adca273425c7b9125005cae8cb9a43d3cbe650d
jin0106 <wjinh93@gmail.com>
2024-12-02 19:21:39 +0900

    v

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       >
         server : {process.env.PLATFORM_ENV} auto deploy222
         version:{version}
-        staging~~~~~
+        staging~@@@@
       </div>
   );
 }
